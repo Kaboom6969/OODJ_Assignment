9a20255178ca74c4906e3fa493383810eb36d30a
BGORE <leezhihao0427@gmail.com>
2026-07-21 00:20:37 +0800

    写了病人类（测试用）

---
+++ src/entities/Patient.java
@@ -0,0 +1,21 @@
+package entities;
+
+import java.lang.reflect.Method;
+
+public class Patient extends User
+{
+
+    public Patient(String[] data)
+    {
+        this(data[0],data[1],data[2]);
+    }
+    public Patient(String id, String name, String password)
+    {
+        super(id,name,password);
+    }
+    @Override
+    public String getIdPrefix() {
+        return "PT";
+    }
+
+}
