3861de837f4a9680ad4bdcb8635442eaadabdf19
BGORE <leezhihao0427@gmail.com>
2026-07-21 00:22:49 +0800

    测试

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Tools.FileDataHandler;
+import entities.Patient;
+
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-void main() {
-    System.out.println("Test");
+void main() throws FileNotFoundException {
+    FileDataHandler myHanlder = new FileDataHandler("C:/Users/leezh/IdeaProjects/OODJ Assignment/data/Patient.txt");
+    Patient myPatient = myHanlder.getEntity(2,Patient::new);
+    IO.println(myPatient);
 }
