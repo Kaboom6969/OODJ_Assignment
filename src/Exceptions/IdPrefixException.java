03cf098d652ff3c63443efe3b5961a343cf57061
BGORE <leezhihao0427@gmail.com>
2026-07-21 03:42:09 +0800

    加了一个新异常父类

---
+++ src/Exceptions/IdPrefixException.java
@@ -0,0 +1,28 @@
+﻿package Exceptions;
+
+public class IdPrefixException extends RuntimeException
+{
+    public IdPrefixException()
+    {
+    }
+
+    public IdPrefixException(String message)
+    {
+        super(message);
+    }
+
+    public IdPrefixException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public IdPrefixException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public IdPrefixException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
