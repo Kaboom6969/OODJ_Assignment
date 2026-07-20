293823b8ab0a58b4d4002942978622c7500512ab
BGORE <leezhihao0427@gmail.com>
2026-07-21 00:25:22 +0800

    添加了新功能 优化了getEntity方法 添加了getFileRow方法 添加了cacheFile方法（未测试） 添加了DeleteRow方法（未测试）

--- src/Tools/FileDataHandler.java
+++ src/Tools/FileDataHandler.java
@@ -1,16 +1,19 @@
-﻿package Tools;
+package Tools;
 
+import Exceptions.IdPrefixNotMatchException;
+import Exceptions.ReaderPrepareFailedException;
 import entities.BaseEntity;
 
 import java.io.*;
+import java.util.List;
 import java.util.function.Function;
 
 public class FileDataHandler
 {
-    private File file;
+    private final File file;
     private BufferedReader fileReader;
 
-    public void prepareReader() throws FileNotFoundException
+    public void prepareReader()
     {
         if (fileReader != null)
         {
@@ -18,26 +21,23 @@ public class FileDataHandler
             {
                 fileReader.close();
             }
-            catch (IOException e)
-            {
-                throw new RuntimeException(e);
-            }
+            catch (IOException _){}
         }
-        fileReader = new BufferedReader(new FileReader(this.file));
+        try
+        {
+            fileReader = new BufferedReader(new FileReader(this.file));
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("File:" + file.getName() + "not Found!");
+            throw new ReaderPrepareFailedException();
+        }
     }
 
     public FileDataHandler(String filePath) throws FileNotFoundException
     {
         this.file = new File(filePath);
-        try
-        {
-            prepareReader();
-        }
-        catch (FileNotFoundException e)
-        {
-            IO.println("File:" + file.getName() + "not Found!");
-            throw new FileNotFoundException(e.getMessage());
-        }
+        prepareReader();
     }
 
                                                             //类型擦除导致只能让T自己把方法丢给这里
@@ -48,8 +48,53 @@ public class FileDataHandler
             prepareReader();
             for(int i = 0;i < rowInFile - 1;i ++) { fileReader.readLine();}
             String[] data = fileReader.readLine().split("\\|");
-            return constructorFunctionForT.apply(data);
+            try
+            {
+                return constructorFunctionForT.apply(data);
+            }
+            catch (IdPrefixNotMatchException e)
+            {
+                System.err.println("Please Check Your File,The idPrefix didn't match!\n" + e.getMessage());
+                return null;
+            }
         }
         catch (IOException _) {return null;}
+    }
+    public List<String> cacheFile() throws IOException
+    {
+        prepareReader();
+        return this.fileReader.readAllLines();
+    }
+    public int getFileRow()
+    {
+        prepareReader();
+        try
+        {
+            int row = 0;
+            while (this.fileReader.readLine() != null)
+            {
+                row += 1;
+            }
+            return row;
+        }
+        catch (IOException e)
+        {
+            throw new UncheckedIOException(e);
+        }
+    }
+    public void DeleteRow (int row)
+    {
+        if (row+1 > getFileRow()) throw new IllegalArgumentException("row should not be bigger than file row");
+        List<String> originalFile = null;
+        try
+        {
+            originalFile = cacheFile();
+        }
+        catch (IOException e)
+        {
+            System.err.println("Error while delete row");
+        }
+        if (originalFile == null) return;
+        originalFile.remove(row + 1);
     }
 }
