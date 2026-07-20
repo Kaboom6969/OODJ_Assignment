666afcbbe192591a42776d419af31e7057d21b36
BGORE <leezhihao0427@gmail.com>
2026-07-21 00:20:19 +0800

    写了两个异常类

---
+++ src/Exceptions/IdPrefixNotMatchException.java
@@ -0,0 +1,9 @@
+﻿package Exceptions;
+
+public class IdPrefixNotMatchException extends RuntimeException
+{
+    public IdPrefixNotMatchException() {super();}
+    public IdPrefixNotMatchException(String message) {super(message);}
+    public IdPrefixNotMatchException(Throwable cause) {super(cause);}
+    public IdPrefixNotMatchException(String message,Throwable cause){super(message,cause);}
+}
---
+++ src/Exceptions/ReaderPrepareFailedException.java
@@ -0,0 +1,29 @@
+﻿package Exceptions;
+
+public class ReaderPrepareFailedException extends RuntimeException
+{
+    public ReaderPrepareFailedException()
+    {
+        super();
+    }
+
+    public ReaderPrepareFailedException(String message)
+    {
+        super(message);
+    }
+
+    public ReaderPrepareFailedException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public ReaderPrepareFailedException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public ReaderPrepareFailedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
