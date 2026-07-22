73efff126a6d493282ed74a8dc4a6a20e508e360
BGORE <leezhihao0427@gmail.com>
2026-07-22 21:32:05 +0800

    新增了UserWithDetails类 此类形成了对User的扩充（例如生日，性别）

---
+++ src/entities/UserWithDetails.java
@@ -0,0 +1,64 @@
+﻿package entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+
+public abstract class UserWithDetails extends User
+{
+    public enum Gender
+    {
+        MALE,FEMALE,UNKNOWN
+    }
+    private LocalDate dateOfBirth;
+    private Gender gender;
+
+    public UserWithDetails(String id,String name,String password,Gender gender,LocalDate dateOfBirth)
+    {
+        super(id,name,password);
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+    }
+    public UserWithDetails(String[] data)
+    {
+        super(data);
+        this.gender = Gender.valueOf(data[3].toUpperCase().trim());
+        this.dateOfBirth = LocalDate.parse(data[4],LocalDateParser.getDateFormatterAuto(data[4]));
+    }
+
+}
+class LocalDateParser
+{
+    public static DateTimeFormatter getDateFormatterAuto(String date)
+    {
+        char delimiter = '\0';
+        byte delimiterCount = 0;
+        int lastDelimiterIndex = -1;
+        for(int i = 0; i < date.length(); i++)
+        {
+            if (delimiterCount > 2 || delimiterCount < 0) throw new DateTimeParseException("Date Format is incorrect",date,i);
+            if (!Character.isDigit(date.charAt(i)) && !Character.isLetter(date.charAt(i)) && delimiter == '\0')
+            {
+                delimiter = date.charAt(i);
+                delimiterCount++;
+                lastDelimiterIndex = i;
+                continue;
+            }
+            if (delimiter != '\0' && date.charAt(i) == delimiter)
+            {
+                delimiterCount++;
+                lastDelimiterIndex = i;
+                continue;
+            }
+            if (delimiter != '\0' && !Character.isDigit(date.charAt(i)))
+            {
+                throw new DateTimeParseException("Date Format is incorrect",date,i);
+            }
+
+        }
+        if (delimiterCount != 2) throw new DateTimeParseException("Date Format is incorrect",date,lastDelimiterIndex);
+        return DateTimeFormatter.ofPattern("y"+delimiter+"M"+delimiter+"d");
+    }
+}
+
