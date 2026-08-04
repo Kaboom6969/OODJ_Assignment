4dd3b7cce056fa4cb6f806a2191a587977365314
BGORE <leezhihao0427@gmail.com>
2026-08-05 02:46:43 +0800

    给Main方法更新了新的测试代码

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import Exceptions.IdNotFoundException;
+import Exceptions.IdRepeatedException;
+import Tools.EntityHandler;
 import Tools.FileDataHandler;
+import entities.BaseEntity;
 import entities.Patient;
+import entities.UserWithDetails;
 
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-void main() throws FileNotFoundException {
-    FileDataHandler myHanlder = new FileDataHandler("C:/Users/leezh/IdeaProjects/OODJ Assignment/data/Patient.txt");
-    Patient myPatient = myHanlder.getEntity(2,Patient::new);
+void main() throws IdRepeatedException, IdNotFoundException
+{
+    BaseEntity.setIdNumberWidth(4);
+    FileDataHandler myHandlder = new FileDataHandler("C:/Users/leezh/IdeaProjects/OODJ Assignment/data/Patient.txt");
+    EntityHandler eh = new EntityHandler(myHandlder);
+    Patient myPatient = (Patient) eh.getEntity(0);
     IO.println(myPatient);
+    Patient myPatient2 = new Patient("PT0002", "Kek", "YYYY", UserWithDetails.Gender.MALE, "2004/09/03");
+    eh.updateEntity(myPatient2);
 }
