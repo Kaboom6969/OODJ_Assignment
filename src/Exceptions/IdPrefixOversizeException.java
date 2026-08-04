38e4db23617ea9490c13156ec444a9200ef285fb
BGORE <leezhihao0427@gmail.com>
2026-08-05 01:27:17 +0800

    新增了一个异常，用于id数量大于预定id宽度

---
+++ src/Exceptions/IdPrefixOversizeException.java
@@ -0,0 +1,28 @@
+package Exceptions;
+
+public class IdPrefixOversizeException extends IdPrefixException
+{
+    public IdPrefixOversizeException(String message)
+    {
+        super(message);
+    }
+
+    public IdPrefixOversizeException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public IdPrefixOversizeException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public IdPrefixOversizeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public IdPrefixOversizeException()
+    {
+    }
+}
