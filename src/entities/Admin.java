7d7a68595c519aca04c208c6e4d5506c9e4f4061
BGORE <leezhihao0427@gmail.com>
2026-08-05 02:46:15 +0800

    新增了Admin类（继承自User类）

---
+++ src/entities/Admin.java
@@ -0,0 +1,27 @@
+﻿package entities;
+
+public class Admin extends User
+{
+    public static final String PREFIX = "AD";
+    public Admin(String id, String name, String password)
+    {
+        super(id, name, password);
+    }
+
+    public Admin(String[] fileData)
+    {
+        super(fileData);
+    }
+
+    @Override
+    public String getIdPrefix()
+    {
+        return PREFIX;
+    }
+
+    @Override
+    public String toFileData()
+    {
+        return this.getId() + "|" + this.getName() + "|" + this.getPassword();
+    }
+}
