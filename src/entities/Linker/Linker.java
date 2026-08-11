ddbea9568df7e87edadfee8f11b284eee18fd2ac
BGORE <leezhihao0427@gmail.com>
2026-08-12 04:32:54 +0800

    Linker 给我方便的

---
+++ src/entities/Linker/Linker.java
@@ -0,0 +1,15 @@
+﻿package entities.Linker;
+
+import entities.BaseEntity.BaseEntity;
+
+public class Linker
+{
+    public final String first;
+    public final String second;
+
+    public Linker(String first, String second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+}
---
+++ src/entities/Linker/LinkerManager.java
@@ -0,0 +1,46 @@
+﻿package entities.Linker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkerManager
+{
+    public List<Linker> linkers;
+
+    public LinkerManager()
+    {
+        linkers = new ArrayList<Linker>();
+    }
+
+    public LinkerManager(List<Linker> linkers)
+    {
+        this.linkers = linkers;
+    }
+
+    public List<String> findBasedOnFirst(String first)
+    {
+        List<String> ans = new ArrayList<>();
+        for (Linker linker : linkers)
+        {
+            if (linker.first.equals(first))
+            {
+                ans.add(linker.second);
+            }
+        }
+        if (ans.isEmpty()) return null;
+        return ans;
+    }
+    public List<String> findBasedOnSecond(String second)
+    {
+        List<String> ans = new ArrayList<>();
+        for (Linker linker : linkers)
+        {
+            if (linker.second.equals(second))
+            {
+                ans.add(linker.first);
+            }
+        }
+        if (ans.isEmpty()) return null;
+        return ans;
+    }
+}
