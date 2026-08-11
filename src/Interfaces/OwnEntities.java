b6139fc925f89026f06ef2cf57761f563c26a1d9
BGORE <leezhihao0427@gmail.com>
2026-08-12 04:33:13 +0800

    你好像一点用都没有

---
+++ src/Interfaces/OwnEntities.java
@@ -0,0 +1,16 @@
+﻿package Interfaces;
+
+import Iterator.LazyEntityList;
+import entities.BaseEntity.BaseEntity;
+
+public interface OwnEntities<T extends BaseEntity>
+{
+    public LazyEntityList<T> getEntities();
+
+    public void setEntities(LazyEntityList<T> entities);
+    public T getEntity(int index);
+
+    public T getEntity(String id);
+
+    public void setEntity(int index, T entity);
+}
