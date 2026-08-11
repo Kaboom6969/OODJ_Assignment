b8bfc074608b8e6ff7be087dce0cc2239e146eea
BGORE <leezhihao0427@gmail.com>
2026-08-12 04:34:40 +0800

    两个新异常

---
+++ src/Exceptions/LazyListEntityCantGetException.java
@@ -0,0 +1,28 @@
+﻿package Exceptions;
+
+public class LazyListEntityCantGetException extends LazyListException
+{
+    public LazyListEntityCantGetException(String message)
+    {
+        super(message);
+    }
+
+    public LazyListEntityCantGetException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public LazyListEntityCantGetException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public LazyListEntityCantGetException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public LazyListEntityCantGetException()
+    {
+    }
+}
---
+++ src/Exceptions/LazyListException.java
@@ -0,0 +1,28 @@
+﻿package Exceptions;
+
+public class LazyListException extends RuntimeException
+{
+    public LazyListException(String message)
+    {
+        super(message);
+    }
+
+    public LazyListException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public LazyListException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public LazyListException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public LazyListException()
+    {
+    }
+}
