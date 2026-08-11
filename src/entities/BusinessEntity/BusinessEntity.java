93217db8426b5876b6f9480dc2aadcaf31e1f5d2
BGORE <leezhihao0427@gmail.com>
2026-08-12 04:33:26 +0800

    BusinessEntity，ya

---
+++ src/entities/BusinessEntity/BusinessEntity.java
@@ -0,0 +1,13 @@
+﻿package entities.BusinessEntity;
+
+import entities.BaseEntity.BaseEntity;
+
+public abstract class BusinessEntity<T extends BaseEntity>
+{
+    protected T self;
+
+    public BusinessEntity(T self)
+    {
+        this.self = self;
+    }
+}
