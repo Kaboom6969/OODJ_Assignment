5f2ba383bebdd3687ab9d0656243e86ea3aa94a0
BGORE <leezhihao0427@gmail.com>
2026-08-13 01:44:47 +0800

    部分修复

--- src/Tools/HospitalEntityAllocator.java
+++ src/Tools/HospitalEntityAllocator.java
@@ -1,12 +1,14 @@
 package Tools;
 
-import Exceptions.*;
+import Exceptions.EntityExceptions.EntityNotFoundException;
+import Exceptions.EntityExceptions.EntityNotMatchException;
+import Exceptions.EntityExceptions.EntityRepeatedException;
+import Exceptions.IdPrefixExceptions.IdPrefixNotFoundException;
+import Exceptions.IdPrefixExceptions.IdPrefixNotMatchException;
 import Interfaces.ConvertToFileData;
-import Interfaces.OwnEntities;
+import Tools.FileHandler.FileDataHandler;
 import entities.BaseEntity.*;
-import entities.BusinessEntity.BusinessEntity;
 import entities.BusinessEntity.Department;
-import entities.Linker.LinkerManager;
 
 import java.lang.reflect.Field;
 import java.nio.file.Path;
@@ -17,12 +19,12 @@ import java.util.Map;
 public class HospitalEntityAllocator
 {
     public record FilePrefixMatchRecord(boolean isAllMatch,String propertiesName){}
-    private Map<String,FileDataHandler> prefixFileMap;
+    private Map<String, FileDataHandler> prefixFileMap;
     private EntityFile adminEntityFile = new EntityFile(Admin.PREFIX);
     private EntityFile patientEntityFile = new EntityFile(Patient.PREFIX);
-    private EntityFile doctorEntityFile = new EntityFile(Doctor.PREFIX);
+    private EntityFile doctorEntityFile = new EntityFile(DoctorToFile.PREFIX);
 
-    private EntityFile DepartmentEntityFile = new EntityFile(DepartmentToFile.PREFIX);
+    private EntityFile departmentEntityFile = new EntityFile(DepartmentToFile.PREFIX);
     //private EntityHandler entityHandler;
 
     public HospitalEntityAllocator(Path adminPath, Path patientPath, Path doctorPath, Path DepartmentPath, Path DepartmentDoctorLinkPath)
@@ -30,8 +32,8 @@ public class HospitalEntityAllocator
         adminEntityFile.mainFile = new FileDataHandler(adminPath);
         patientEntityFile.mainFile = new FileDataHandler(patientPath);
         doctorEntityFile.mainFile = new FileDataHandler(doctorPath);
-        DepartmentEntityFile.mainFile = new FileDataHandler(DepartmentPath);
-        DepartmentEntityFile.linkFiles.set(0,new FileDataHandler(DepartmentDoctorLinkPath));
+        departmentEntityFile.mainFile = new FileDataHandler(DepartmentPath);
+        departmentEntityFile.linkFiles.set(0,new FileDataHandler(DepartmentDoctorLinkPath));
         prefixFileMap = new HashMap<String, FileDataHandler>();
         FilePrefixMatchRecord filePrefixMatchRecord = classInit();
         if (!filePrefixMatchRecord.isAllMatch())
@@ -98,9 +100,8 @@ public class HospitalEntityAllocator
 
     public Department getDepartment(String id)
     {
-        DepartmentToFile department = getEntity(id);
-        List<String> doctorIds = new EntityHandler(DepartmentEntityFile.linkFiles.getFirst()).getLinker().findBasedOnSecond(id);
-        return new Department(department,doctorIds,doctorEntityFile.mainFile);
+        List<String> doctorIds = new EntityHandler(departmentEntityFile.linkFiles.getFirst()).getLinker().findBasedOnSecond(id);
+        return new Department(id,departmentEntityFile.mainFile,doctorIds,doctorEntityFile.mainFile);
     }
 
     public <T extends BaseEntity> T getEntity(String id)
