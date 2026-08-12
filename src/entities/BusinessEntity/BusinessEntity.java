af33e891f50b9d5b5efbd63d34664175ae10581c
BGORE <leezhihao0427@gmail.com>
2026-08-13 01:40:31 +0800

    重构BusinessEntity让他自己拥有的对象(self)变成lazyEntity

--- src/entities/BusinessEntity/BusinessEntity.java
+++ src/entities/BusinessEntity/BusinessEntity.java
@@ -1,13 +1,18 @@
 ﻿package entities.BusinessEntity;
 
+import Interfaces.ConvertToFileData;
+import entities.LazyEntity.LazyEntity;
+import Tools.EntityHandler;
+import Tools.FileHandler.FileDataHandler;
 import entities.BaseEntity.BaseEntity;
 
-public abstract class BusinessEntity<T extends BaseEntity>
+public abstract class BusinessEntity<T extends BaseEntity & ConvertToFileData>
 {
-    protected T self;
+    private FileDataHandler selfFile;
+    protected LazyEntity<T> self;
 
-    public BusinessEntity(T self)
+    public BusinessEntity(String id,FileDataHandler selfFile)
     {
-        this.self = self;
+        this.self = new LazyEntity<T>(id,new EntityHandler(selfFile));
     }
 }
