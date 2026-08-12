ccb3ba30d54027c012924acc555babb7dad7af2d
BGORE <leezhihao0427@gmail.com>
2026-08-13 01:43:50 +0800

    制作了LazyEntity类，来用于懒加载

---
+++ src/entities/LazyEntity/LazyEntity.java
@@ -0,0 +1,37 @@
+﻿package entities.LazyEntity;
+
+import Exceptions.LazyEntityExceptions.LazyEntityCantGetException;
+import Interfaces.ConvertToFileData;
+import Tools.EntityHandler;
+import entities.BaseEntity.BaseEntity;
+
+public class LazyEntity<T extends BaseEntity & ConvertToFileData>
+{
+    private EntityHandler entityHandler;
+    private String id;
+    private T self;
+
+
+    public LazyEntity(String id, EntityHandler entityHandler)
+    {
+        this.self = null;
+        this.id = id;
+        this.entityHandler = entityHandler;
+    }
+
+    public T getSelf()
+    {
+        if (self == null) self = entityHandler.getEntity(id);
+        if (self == null) throw new LazyEntityCantGetException("Entity is not file in this file:"+entityHandler.getFileName());
+        return self;
+    }
+
+    public String getId() {return id;}
+    public void setSelf(T entity)
+    {
+        this.id = entity.getId();
+        this.self = entity;
+    }
+
+
+}
