df8e2db39a5542d048168c370151d4fa6eb70953
BGORE <leezhihao0427@gmail.com>
2026-08-13 01:39:23 +0800

    新增了新的LazyEntity异常还有移动文件

---
+++ src/Exceptions/LazyEntityExceptions/LazyEntityException.java
@@ -0,0 +1,28 @@
+﻿package Exceptions.LazyEntityExceptions;
+
+public class LazyEntityException extends RuntimeException
+{
+    public LazyEntityException(String message)
+    {
+        super(message);
+    }
+
+    public LazyEntityException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public LazyEntityException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public LazyEntityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public LazyEntityException()
+    {
+    }
+}
---
+++ src/Exceptions/LazyEntityExceptions/LazyEntityListExceptions/LazyEntityListEntityCantGetException.java
@@ -0,0 +1,28 @@
+﻿package Exceptions.LazyEntityExceptions.LazyEntityListExceptions;
+
+public class LazyEntityListEntityCantGetException extends LazyEntityListException
+{
+    public LazyEntityListEntityCantGetException(String message)
+    {
+        super(message);
+    }
+
+    public LazyEntityListEntityCantGetException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public LazyEntityListEntityCantGetException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public LazyEntityListEntityCantGetException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public LazyEntityListEntityCantGetException()
+    {
+    }
+}
