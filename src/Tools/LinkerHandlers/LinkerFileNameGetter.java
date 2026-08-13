1368e4e46431512209c91fb5a479be24def53c8a
BGORE <leezhihao0427@gmail.com>
2026-08-13 21:21:46 +0800

    新增了一个新的类

---
+++ src/Tools/LinkerHandlers/LinkerFileNameGetter.java
@@ -0,0 +1,19 @@
+﻿package Tools.LinkerHandlers;
+
+import Tools.EntityConvertManager;
+import Tools.FileHandler.FileDataHandler;
+import entities.Linker.Linker;
+
+public class LinkerFileNameGetter
+{
+    public static String getFileName(Linker linker)
+    {
+        String firstId = linker.first;
+        String SecondId = linker.second;
+        String firstPrefix = FileDataHandler.prefixFinder(firstId);
+        String secondPrefix = FileDataHandler.prefixFinder(SecondId);
+        String firstClassName = EntityConvertManager.getEntityMap().get(firstPrefix).getSimpleName();
+        String secondClassName = EntityConvertManager.getEntityMap().get(secondPrefix).getSimpleName();
+        return firstClassName + "_" + secondClassName + "_" + "linker.txt";
+    }
+}
