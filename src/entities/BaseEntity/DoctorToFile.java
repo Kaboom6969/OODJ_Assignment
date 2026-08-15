8b6ed285f2dffebf2cfc38c1e6e61b54e715038c
BGORE <leezhihao0427@gmail.com>
2026-08-15 16:21:08 +0800

    修改有关于doctor的连接问题

--- src/entities/BaseEntity/DoctorToFile.java
+++ src/entities/BaseEntity/DoctorToFile.java
@@ -7,31 +7,23 @@ import java.time.LocalDate;
 public class DoctorToFile extends UserWithDetails implements ConvertToFileData
 {
     public static final String PREFIX = "DT";
-    private String departmentId;
 
-    public String getDepartmentId()
-    {
-        return departmentId;
-    }
 
     public DoctorToFile(String id) {super(id);}
 
-    public DoctorToFile(String id, String name, String password, Gender gender, LocalDate dateOfBirth, String departmentId)
+    public DoctorToFile(String id, String name, String password, Gender gender, LocalDate dateOfBirth)
     {
         super(id, name, password, gender, dateOfBirth);
-        this.departmentId = departmentId;
     }
 
     public DoctorToFile(String[] data)
     {
         super(data);
-        this.departmentId = data[5];
     }
 
     public DoctorToFile(String id, String name, String password, Gender gender, String dateOfBirth, String departmentId)
     {
         super(id, name, password, gender, dateOfBirth);
-        this.departmentId = departmentId;
     }
 
     @Override
@@ -48,7 +40,6 @@ public class DoctorToFile extends UserWithDetails implements ConvertToFileData
                 this.getName() + "|" +
                 this.getPassword() + "|" +
                 this.getGender() + "|" +
-                this.getDateOfBirth() + "|" +
-                this.getDepartmentId() + "\n";
+                this.getDateOfBirth() + "\n";
     }
 }
