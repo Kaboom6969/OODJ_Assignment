97b6ef417a3da90d31ae558d2524c2439f311900
BGORE <leezhihao0427@gmail.com>
2026-08-30 18:48:44 +0800

    给linker 添加了基于keylocation得到资料的方法

--- src/main/java/entities/Linker/Linker.java
+++ src/main/java/entities/Linker/Linker.java
@@ -1,7 +1,10 @@
 package entities.Linker;
 
 import Exceptions.IdPrefixExceptions.IdPrefixReapeatedException;
+import Exceptions.LinkerExceptions.LinkerException;
+import Exceptions.LinkerExceptions.LinkerNotFoundException;
 import Tools.FileHandler.FileDataHandler;
+import Tools.PrefixHandler.PrefixFinder;
 
 import java.util.Objects;
 
@@ -10,10 +13,22 @@ public class Linker
     public final String first;
     public final String second;
 
+    public String getData(LinkerManager.KeyLocation keyLocation)
+    {
+        switch (keyLocation)
+        {
+            case FIRST:
+                return this.first;
+            case SECOND:
+                return this.second;
+            default:
+                throw new LinkerException("Cannot find the data");
+        }
+    }
     public Linker(String first, String second)
     {
         this.first = first;
-        if (FileDataHandler.prefixFinder(first).equals(FileDataHandler.prefixFinder(second)))
+        if (PrefixFinder.findPrefix(first).equals(PrefixFinder.findPrefix(second)))
         {
             throw new IdPrefixReapeatedException("first and second prefixes are the same");
         }
