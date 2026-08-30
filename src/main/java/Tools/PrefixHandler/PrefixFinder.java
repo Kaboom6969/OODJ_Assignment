99768c8cfc86247009c937a00846529a6f28600d
BGORE <leezhihao0427@gmail.com>
2026-08-30 18:45:11 +0800

    把前缀抽取方法弄成一个新的类

---
+++ src/main/java/Tools/PrefixHandler/PrefixFinder.java
@@ -0,0 +1,30 @@
+﻿package Tools.PrefixHandler;
+
+import Exceptions.IdPrefixExceptions.IdPrefixNotFoundException;
+
+public class PrefixFinder
+{
+    private static final int MAXIMUM_PREFIX_LENGTH = 10;
+    public static String findPrefix(String id)
+    {
+        if (id == null || id.isEmpty()) throw new IdPrefixNotFoundException("The id is empty!");
+        char[] prefix = new char[MAXIMUM_PREFIX_LENGTH];
+        int prefixPointer = 0;
+        for (int i = 0;i < id.length(); i ++)
+        {
+            if (Character.isLetter(id.charAt(i)))
+            {
+                if (prefixPointer >= MAXIMUM_PREFIX_LENGTH)
+                    throw new IllegalArgumentException
+                            ("prefix length is larger than maximum prefix length\n" +
+                                    "Please check your file Or change the maximum prefix length");
+                prefix[prefixPointer] = id.charAt(i);
+                prefixPointer ++;
+                continue;
+            }
+            if (Character.isDigit(id.charAt(i))) {break;}
+        }
+        if (prefixPointer == 0) throw new IdPrefixNotFoundException("No Prefix Found in this id!");
+        return new String(prefix,0,prefixPointer);
+    }
+}
