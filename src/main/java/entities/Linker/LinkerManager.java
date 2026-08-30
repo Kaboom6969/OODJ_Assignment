eaf7c0eeeb41a905ab047ca3dd1fa3c63f704b58
BGORE <leezhihao0427@gmail.com>
2026-08-30 18:47:55 +0800

    添加了新的枚举来方便说明位置，还有添加了四个新的方法 分别是切换keylocation 根据keylocation得到类 根据key得到keylocation 过滤linkermanager根据key

--- src/main/java/entities/Linker/LinkerManager.java
+++ src/main/java/entities/Linker/LinkerManager.java
@@ -6,19 +6,29 @@ import Exceptions.LinkerExceptions.LinkerNotFoundException;
 import Exceptions.LinkerExceptions.LinkerRepeatedException;
 import Interfaces.ConvertToFileData;
 import Tools.EntityConvertManager;
-import Tools.FileHandler.FileDataHandler;
+import Tools.PrefixHandler.PrefixFinder;
 import entities.BaseEntity.BaseEntity;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LinkerManager implements ConvertToFileData
 {
+    public enum KeyLocation
+    {
+        FIRST,SECOND, NOT_FOUND
+    }
     private Class<? extends BaseEntity> classFirst;
     private Class<? extends BaseEntity> classSecond;
-    public List<Linker> linkers;
+    private List<Linker> linkers;
 
-    public LinkerManager()
+    public List<Linker> getLinkers()
+    {
+        return Collections.unmodifiableList(linkers);
+    }
+
+    private LinkerManager()
     {
         linkers = new ArrayList<Linker>();
     }
@@ -37,13 +47,14 @@ public class LinkerManager implements ConvertToFileData
         this.linkers = linkers;
     }
 
-    public void addLinker(Linker linker)
+    public boolean addLinker(Linker linker)
     {
         for(Linker l : linkers)
         {
-            if(l.equals(linker)) throw new LinkerRepeatedException("Linker already exists");
+            if(l.equals(linker)) return false;
         }
         linkers.add(linker);
+        return true;
     }
 
     public void removeLinker(Linker linker)
@@ -65,16 +76,33 @@ public class LinkerManager implements ConvertToFileData
     {
         linkers.clear();
     }
-
-    public List<String> findBasedOnKey(String key)
+    public LinkerManager filterBasedOnKey(String key)
+    {
+        KeyLocation keyLocation = getKeyLocation(key);
+        List<Linker> linkersAfterFiltered = new ArrayList<>();
+        for (Linker linker : linkers)
+        {
+            if (linker.getData(keyLocation).equals(key)) linkersAfterFiltered.add(linker);
+        }
+        return new LinkerManager(classFirst, classSecond, linkersAfterFiltered);
+    }
+    public KeyLocation getKeyLocation(String key)
     {
         String prefixFirst = EntityConvertManager.getPrefixMap().get(classFirst);
         String prefixSecond = EntityConvertManager.getPrefixMap().get(classSecond);
-        String keyPrefix = FileDataHandler.prefixFinder(key);
+        String keyPrefix = PrefixFinder.findPrefix(prefixFirst);
         if (prefixFirst == null || prefixSecond == null) throw new MapEmptyException("ConvertMap is Empty");
-        if (keyPrefix.equals(prefixFirst)) {return findBasedOnFirst(key);}
-        else if (keyPrefix.equals(prefixSecond)) {return findBasedOnSecond(key);}
-        throw new IdPrefixNotFoundException("key prefix is not matched in this linker!");
+        if (keyPrefix.equals(prefixFirst)) {return KeyLocation.FIRST;}
+        else if (keyPrefix.equals(prefixSecond)) {return KeyLocation.SECOND;}
+        else return KeyLocation.NOT_FOUND;
+    }
+    public List<String> findBasedOnKey(String key)
+    {
+        KeyLocation keyLocation = getKeyLocation(key);
+        if (keyLocation == KeyLocation.FIRST) {return findBasedOnFirst(key);}
+        else if (keyLocation == KeyLocation.SECOND) {return findBasedOnSecond(key);}
+        else if (keyLocation == KeyLocation.NOT_FOUND) throw new IdPrefixNotFoundException("key prefix is not matched in this linker!");
+        else throw new RuntimeException("Unrecognized KeyLocation");
     }
     public List<String> findBasedOnFirst(String first)
     {
@@ -89,7 +117,33 @@ public class LinkerManager implements ConvertToFileData
         if (ans.isEmpty()) return new ArrayList<>();
         return ans;
     }
+    public static KeyLocation switchKeyLocation(KeyLocation keyLocation)
+    {
+        switch (keyLocation)
+        {
+            case FIRST: return KeyLocation.SECOND;
+            case SECOND: return KeyLocation.FIRST;
+            case NOT_FOUND: return KeyLocation.NOT_FOUND;
+            default: throw new RuntimeException("Unrecognized KeyLocation");
+        }
+    }
 
+    public Class<? extends BaseEntity> getClassBasedOnKeyLocation(KeyLocation keyLocation)
+    {
+        switch (keyLocation)
+        {
+            case FIRST :
+                return classFirst;
+            case SECOND :
+                return classSecond;
+            case NOT_FOUND :
+                throw new IdPrefixNotFoundException
+                (
+                    "Please give a workable Location"
+                );
+            default: throw new RuntimeException("Unrecognized KeyLocation");
+        }
+    }
     public List<String> findBasedOnSecond(String second)
     {
         List<String> ans = new ArrayList<>();
