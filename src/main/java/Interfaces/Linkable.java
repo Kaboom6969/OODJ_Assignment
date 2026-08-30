cbec7c57d39a679196b5c674b591e8d352b008ab
BGORE <leezhihao0427@gmail.com>
2026-08-30 18:45:40 +0800

    修改接口以方便我完成重构

--- src/main/java/Interfaces/Linkable.java
+++ src/main/java/Interfaces/Linkable.java
@@ -1,10 +1,16 @@
 package Interfaces;
 
 import Tools.FileHandler.FileDataHandler;
+import entities.Linker.Linker;
+import entities.Linker.LinkerManager;
+
+import java.util.List;
 
 public interface Linkable
 {
     public FileDataHandler getLinkFile();
 
     public void setLinkFile(FileDataHandler linkFile);
+
+    public List<LinkerManager> getLinkerManager();
 }
