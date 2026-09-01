bf92d570566bc941e1c36b9977db912b7a2cea57
BGORE <leezhihao0427@gmail.com>
2026-09-02 02:33:30 +0800

    给lazyEntity加上了对比自身是否被修改的能力

--- src/main/java/entities/LazyEntity/LazyEntity.java
+++ src/main/java/entities/LazyEntity/LazyEntity.java
@@ -6,11 +6,14 @@ import Interfaces.ConvertToFileData;
 import Tools.EntityHandler;
 import entities.BaseEntity.BaseEntity;
 
+import javax.print.attribute.standard.Copies;
+
 public class LazyEntity<T extends BaseEntity & ConvertToFileData>
 {
     private EntityHandler entityHandler;
     private String id;
     private T self;
+    private String selfBackup;
 
 
     public LazyEntity(String id, EntityHandler entityHandler)
@@ -27,17 +30,32 @@ public class LazyEntity<T extends BaseEntity & ConvertToFileData>
         this.entityHandler = entityHandler;
     }
 
+    public void updateBackup()
+    {
+        this.selfBackup = self.toFileData();
+    }
+
+    public boolean isSelfAlrChanged()
+    {
+        if (self == null) return false;
+        return !self.toFileData().equals(selfBackup);
+    }
     public T getSelf()
     {
         try
         {
-            if (self == null) self = entityHandler.getEntity(id);
+            if (self == null)
+            {
+                self = entityHandler.getEntity(id);
+                if (self == null) throw new LazyEntityCantGetException("Entity is not file in this file:"+entityHandler.getFileName());
+                selfBackup = self.toFileData();
+            }
         }
         catch (IdPrefixNotMatchException e)
         {
             throw new LazyEntityCantGetException(e);
         }
-        if (self == null) throw new LazyEntityCantGetException("Entity is not file in this file:"+entityHandler.getFileName());
+
         return self;
     }
 
