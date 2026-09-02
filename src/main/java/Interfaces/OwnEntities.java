8e56e408567fe2aa3bacae140483617c7a7a6355
BGORE <leezhihao0427@gmail.com>
2026-09-03 02:36:10 +0800

    给Own 系列添加了父接口，让我方便做东西

--- src/main/java/Interfaces/OwnEntities.java
+++ src/main/java/Interfaces/OwnEntities.java
@@ -5,7 +5,7 @@ import entities.BaseEntity.BaseEntity;
 
 import java.util.List;
 
-public interface OwnEntities
+public interface OwnEntities extends OwnerShip
 {
     public List<LazyEntityList<? extends BaseEntity>> getEntities();
 
--- src/main/java/Interfaces/OwnEntity.java
+++ src/main/java/Interfaces/OwnEntity.java
@@ -1,11 +1,12 @@
 package Interfaces;
 
 import entities.BaseEntity.BaseEntity;
+import entities.LazyEntity.LazyEntity;
 
 import java.util.List;
 
-public interface OwnEntity
+public interface OwnEntity extends OwnerShip
 {
-    public List<? extends BaseEntity> getEntity();
+    public List<LazyEntity<? extends BaseEntity>> getEntity();
 
 }
