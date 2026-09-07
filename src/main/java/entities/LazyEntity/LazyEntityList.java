e3abb1f2d5fa2cd7a4ced6eb8f8848d085de47d2
BGORE <leezhihao0427@gmail.com>
2026-09-08 03:27:23 +0800

    添加了两个新的构造函数，并且添加了重复检测

--- src/main/java/entities/LazyEntity/LazyEntityList.java
+++ src/main/java/entities/LazyEntity/LazyEntityList.java
@@ -1,6 +1,7 @@
 package entities.LazyEntity;
 
 import Exceptions.LazyEntityExceptions.LazyEntityListExceptions.LazyEntityListEntityCantGetException;
+import Exceptions.LazyEntityExceptions.LazyEntityListExceptions.LazyEntityListEntityRepeatedException;
 import Interfaces.ConvertToFileData;
 import Tools.EntityHandler;
 import Tools.FileHandler.FileDataHandler;
@@ -14,6 +15,7 @@ public class LazyEntityList<T extends BaseEntity & ConvertToFileData> implements
     private List<LazyEntity<T>> lazyEntityList;
 
     private EntityHandler entityHandler;
+    private record RepeatReport (boolean isRepeated,List<List<Integer>> RepeatedIdIndex){}
 
 
     public int size()
@@ -44,6 +46,54 @@ public class LazyEntityList<T extends BaseEntity & ConvertToFileData> implements
         }
     }
 
+    public LazyEntityList(List<String> entityIdList, EntityHandler entityFile)
+    {
+        lazyEntityList = new ArrayList<LazyEntity<T>>();
+        this.entityHandler = entityFile;
+        for (String entityId : entityIdList)
+        {
+            lazyEntityList.add(new LazyEntity<>(entityId, entityHandler));
+        }
+    }
+
+    public List<String> getIds()
+    {
+        List<String> ids = new ArrayList<>();
+        for(LazyEntity<T> lazyEntity : lazyEntityList)
+        {
+            ids.add(lazyEntity.getId());
+        }
+        return ids;
+    }
+    private RepeatReport repeatCheck()
+    {
+        HashMap<String,List<Integer>> repeatedMap = new HashMap<>();
+        boolean isRepeated = false;
+        for (int i = 0; i<lazyEntityList.size(); i++)
+        {
+            String entityId = lazyEntityList.get(i).getId();
+            if (!repeatedMap.containsKey(entityId))
+            {
+                repeatedMap.put(entityId, new ArrayList<>());
+            }
+            else {isRepeated = true;}
+            repeatedMap.get(entityId).add(i);
+        }
+        return new RepeatReport(isRepeated,new ArrayList<>(repeatedMap.values().stream().filter(list -> list.size() > 1).toList()));
+    }
+
+    private boolean isRepeated(String entityId,int excludedIndex)
+    {
+        for (LazyEntity<T> lazyEntity : lazyEntityList)
+        {
+            if (lazyEntity.getId().equals(entityId) && !(lazyEntityList.indexOf(lazyEntity)==(excludedIndex)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public T get(int index)
     {
         return lazyEntityList.get(index).getSelf();
@@ -64,29 +114,35 @@ public class LazyEntityList<T extends BaseEntity & ConvertToFileData> implements
     {
         if (id == null) throw new IllegalArgumentException("id cannot be null");
         if (id.isEmpty()) throw new IllegalArgumentException("id cannot be empty");
+        if (isRepeated(id,-1)) throw new LazyEntityListEntityRepeatedException("repeated entity with id " + id);
         lazyEntityList.add(new LazyEntity<>(id, entityHandler));
     }
 
     public void add(LazyEntity<T> entity)
     {
+        if (isRepeated(entity.getId(),-1)) throw new LazyEntityListEntityRepeatedException("repeated entity with id " + entity.getId());
         lazyEntityList.add(entity);
     }
 
     public void add(T entity)
     {
+        if (isRepeated(entity.getId(),-1)) throw new LazyEntityListEntityRepeatedException("repeated entity with id " + entity.getId());
         lazyEntityList.add(new LazyEntity<>(entity,entityHandler));
     }
     public void set(int index,String id)
     {
+        if (isRepeated(id,index)) throw new LazyEntityListEntityRepeatedException("repeated entity with id " + id);
         lazyEntityList.set(index,new LazyEntity<>(id,entityHandler));
     }
     public void set(int index, LazyEntity<T> entity)
     {
+        if (isRepeated(entity.getId(),index)) throw new LazyEntityListEntityRepeatedException("repeated entity with id " + entity.getId());
         lazyEntityList.set(index, entity);
     }
 
     public void set(int index, T entity)
     {
+        if (isRepeated(entity.getId(),index)) throw new LazyEntityListEntityRepeatedException("repeated entity with id " + entity.getId());
         lazyEntityList.set(index,new LazyEntity<>(entity,entityHandler));
     }
 
@@ -97,7 +153,23 @@ public class LazyEntityList<T extends BaseEntity & ConvertToFileData> implements
 
     public void remove(T entity)
     {
-        lazyEntityList.remove(new LazyEntity<>(entity,entityHandler));
+        for (int i = 0; i<lazyEntityList.size(); i++)
+        {
+            if (lazyEntityList.get(i).getSelf().equals(entity)) lazyEntityList.remove(i);
+        }
+    }
+
+    public void remove(String id)
+    {
+        if (id == null) throw new IllegalArgumentException("id cannot be null");
+        if (id.isEmpty()) throw new IllegalArgumentException("id cannot be empty");
+        for (int i = 0; i<lazyEntityList.size(); i++)
+        {
+            if (lazyEntityList.get(i).getSelf().getId().equals(id))
+            {
+                lazyEntityList.remove(i); break;
+            }
+        }
     }
 
     public void remove(int index)
