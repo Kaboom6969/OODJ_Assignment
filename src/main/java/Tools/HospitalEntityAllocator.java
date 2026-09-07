7f8adb9852570aaca959abc7253a07802f946fb0
BGORE <leezhihao0427@gmail.com>
2026-09-07 16:31:22 +0800

    现在实体类不是透过硬编码文件的方式来知道文件位置，而是自动推算

--- src/main/java/Tools/HospitalEntityAllocator.java
+++ src/main/java/Tools/HospitalEntityAllocator.java
@@ -5,13 +5,12 @@ import Exceptions.EntityExceptions.EntityNotFoundException;
 import Exceptions.EntityExceptions.EntityNotMatchException;
 import Exceptions.EntityExceptions.EntityRepeatedException;
 import Exceptions.IdPrefixExceptions.IdPrefixNotFoundException;
-import Exceptions.IdPrefixExceptions.IdPrefixNotMatchException;
-import Exceptions.LinkerExceptions.LinkerNotFoundException;
 import Interfaces.*;
 import Tools.FileHandler.FileDataHandler;
 import Tools.LinkerHandlers.LinkerHandler;
 import Tools.PrefixHandler.PrefixFinder;
 import entities.BaseEntity.*;
+import entities.BaseEntity.Users.DoctorToFile;
 import entities.BusinessEntity.BusinessEntity;
 import entities.BusinessEntity.Department;
 import entities.BusinessEntity.Doctor;
@@ -19,7 +18,6 @@ import entities.LazyEntity.LazyEntity;
 import entities.LazyEntity.LazyEntityList;
 import entities.Linker.Linker;
 import entities.Linker.LinkerManager;
-import jdk.jshell.spi.ExecutionControl;
 
 import java.lang.reflect.Field;
 import java.nio.file.Path;
@@ -33,83 +31,84 @@ public class HospitalEntityAllocator
     public record FilePrefixMatchRecord(boolean isAllMatch,String propertiesName){}
     private Map<String, FileDataHandler> prefixFileMap;
     private Path linkerDirectory;
-    private EntityFile adminEntityFile = new EntityFile(Admin.PREFIX);
-    private EntityFile patientEntityFile = new EntityFile(Patient.PREFIX);
-    private EntityFile doctorEntityFile = new EntityFile(DoctorToFile.PREFIX);
+    private Path entityDirectory;
 
-    private EntityFile departmentEntityFile = new EntityFile(DepartmentToFile.PREFIX);
     //private EntityHandler entityHandler;
 
-    public HospitalEntityAllocator(Path linkerDirectory,Path adminPath, Path patientPath, Path doctorPath, Path DepartmentPath)
+    public HospitalEntityAllocator(Path linkerDirectory,Path entityDirectory)
     {
         this.linkerDirectory = linkerDirectory;
-        adminEntityFile.mainFile = new FileDataHandler(adminPath);
-        patientEntityFile.mainFile = new FileDataHandler(patientPath);
-        doctorEntityFile.mainFile = new FileDataHandler(doctorPath);
-        departmentEntityFile.mainFile = new FileDataHandler(DepartmentPath);
+        this.entityDirectory = entityDirectory;
         prefixFileMap = new HashMap<String, FileDataHandler>();
-        FilePrefixMatchRecord filePrefixMatchRecord = classInit();
-        if (!filePrefixMatchRecord.isAllMatch())
-        {
-            throw new IllegalStateException("Prefix check failed,"+filePrefixMatchRecord.propertiesName+" problem");
-        }
+        classInit();
+//        if (!filePrefixMatchRecord.isAllMatch())
+//        {
+//            throw new IllegalStateException("Prefix check failed,"+filePrefixMatchRecord.propertiesName+" problem");
+//        }
     }
-
-    private FilePrefixMatchRecord classInit()
+    private void classInit()
     {
-        for(Field field : this.getClass().getDeclaredFields())
+        for (Map.Entry<String,Class<? extends BaseEntity>> entityMap : EntityConvertManager.getEntityMap().entrySet())
         {
-            if (field.getType() != EntityFile.class) continue;
-            field.setAccessible(true);
-            EntityFile entityFile = null;
-            try
-            {
-                entityFile = (EntityFile) field.get(this);
-            }
-            catch (IllegalAccessException e)
-            {
-                System.err.printf("%s's fileDataHandler is inaccessible!",field.getName());
-                return new FilePrefixMatchRecord(false, field.getName());
-            }
-            try
-            {
-                String prefixInFile = entityFile.mainFile.findPrefixStrict();
-                if (!prefixInFile.equals(entityFile.prefix))
-                {
-                    System.err.printf
-                            ("Prefix in file: %s is not match the prefix:%s,prefix in file:%s\n",
-                                entityFile.mainFile.getFile().getName(),
-                                entityFile.prefix,
-                                prefixInFile
-                            );
-                    prefixFileMap = null;
-                    return new FilePrefixMatchRecord(false, field.getName());
-                }
-                prefixFileMap.put(entityFile.prefix, entityFile.mainFile);
-            }
-            catch (NullPointerException e)
-            {
-                System.err.printf("%s's properties or it self is null!",field.getName());
-                prefixFileMap = null;
-                return new FilePrefixMatchRecord(false, field.getName());
-            }
-            catch (IdPrefixNotMatchException e)
-            {
-                System.err.printf("%s's file's prefix inside is not matched!",field.getName());
-                prefixFileMap = null;
-                return new FilePrefixMatchRecord(false, field.getName());
-            }
-            catch (IdPrefixNotFoundException e)
-            {
-                System.err.printf("%s's file's prefix not found!",field.getName());
-                prefixFileMap = null;
-                return new FilePrefixMatchRecord(false, field.getName());
-            }
-
-
+            prefixFileMap.put(entityMap.getKey(),new FileDataHandler(entityDirectory.resolve(entityMap.getValue().getSimpleName() + ".txt")));
         }
-        return new FilePrefixMatchRecord(true, null);
     }
+
+//    private FilePrefixMatchRecord classInit()
+//    {
+//        for(Field field : this.getClass().getDeclaredFields())
+//        {
+//            if (field.getType() != EntityFile.class) continue;
+//            field.setAccessible(true);
+//            EntityFile entityFile = null;
+//            try
+//            {
+//                entityFile = (EntityFile) field.get(this);
+//            }
+//            catch (IllegalAccessException e)
+//            {
+//                System.err.printf("%s's fileDataHandler is inaccessible!",field.getName());
+//                return new FilePrefixMatchRecord(false, field.getName());
+//            }
+//            try
+//            {
+//                String prefixInFile = entityFile.mainFile.findPrefixStrict();
+//                if (!prefixInFile.equals(entityFile.prefix))
+//                {
+//                    System.err.printf
+//                            ("Prefix in file: %s is not match the prefix:%s,prefix in file:%s\n",
+//                                entityFile.mainFile.getFile().getName(),
+//                                entityFile.prefix,
+//                                prefixInFile
+//                            );
+//                    prefixFileMap = null;
+//                    return new FilePrefixMatchRecord(false, field.getName());
+//                }
+//                prefixFileMap.put(entityFile.prefix, entityFile.mainFile);
+//            }
+//            catch (NullPointerException e)
+//            {
+//                System.err.printf("%s's properties or it self is null!",field.getName());
+//                prefixFileMap = null;
+//                return new FilePrefixMatchRecord(false, field.getName());
+//            }
+//            catch (IdPrefixNotMatchException e)
+//            {
+//                System.err.printf("%s's file's prefix inside is not matched!",field.getName());
+//                prefixFileMap = null;
+//                return new FilePrefixMatchRecord(false, field.getName());
+//            }
+//            catch (IdPrefixNotFoundException e)
+//            {
+//                System.err.printf("%s's file's prefix not found!",field.getName());
+//                prefixFileMap = null;
+//                return new FilePrefixMatchRecord(false, field.getName());
+//            }
+//
+//
+//        }
+//        return new FilePrefixMatchRecord(true, null);
+//    }
 
     public Department getDepartment(String id)
     {
@@ -117,7 +116,7 @@ public class HospitalEntityAllocator
         List<String> doctorIds = new ArrayList<>();
         if (linkerHandler.getLinkers() != null) doctorIds = linkerHandler.getLinkers().findBasedOnKey(id);
         if(doctorIds == null) doctorIds = new ArrayList<>();
-        return new Department(id,departmentEntityFile.mainFile,doctorIds,doctorEntityFile.mainFile);
+        return new Department(id,prefixFileMap.get(PrefixFinder.findPrefix(id)),doctorIds,prefixFileMap.get(PrefixFinder.findPrefix(DoctorToFile.PREFIX)));
     }
 
     public Doctor getDoctor(String id)
@@ -127,7 +126,7 @@ public class HospitalEntityAllocator
         if (linkerHandler.getLinkers() != null) departmentIds = linkerHandler.getLinkers().findBasedOnKey(id);
         if(departmentIds == null) departmentIds = new ArrayList<>();
         if(departmentIds.size() > 1) throw new RuntimeException("err stub");
-        return new Doctor(id,doctorEntityFile.mainFile,departmentIds.isEmpty() ? null : departmentIds.getFirst(),departmentEntityFile.mainFile);
+        return new Doctor(id,prefixFileMap.get(PrefixFinder.findPrefix(id)),departmentIds.isEmpty() ? null : departmentIds.getFirst(),prefixFileMap.get(DepartmentToFile.PREFIX));
     }
     private <T extends BusinessEntity<?> & OwnerShip & Linkable> void saveLinkers(T businessEntity)
     {
