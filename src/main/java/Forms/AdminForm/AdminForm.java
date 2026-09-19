b4499bd455727a6691f73f1d6992cde774aaa6cc
BGORE <leezhihao0427@gmail.com>
2026-09-19 17:10:05 +0800

    提交初始form

---
+++ src/main/java/Forms/AdminForm/AdminForm.java
@@ -0,0 +1,41 @@
+/*
+ * Created by JFormDesigner on Sat Sep 19 00:36:11 GMT+08:00 2026
+ */
+
+package Forms.AdminForm;
+
+import javax.swing.*;
+import javax.swing.GroupLayout;
+
+/**
+ * @author leezh
+ */
+public class AdminForm extends JFrame {
+    public AdminForm() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 588, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 399, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
