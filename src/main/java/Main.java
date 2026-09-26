209e87fc9001cf85de402b6e6af02483953e6c72
BGORE <leezhihao0427@gmail.com>
2026-09-26 22:15:06 +0800

    给我的gui 加上一个美美的皮肤

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,32 @@ import Forms.LoginForm.LoginForm;
 import Tools.HospitalEntityAllocator;
 import entities.BaseEntity.BaseEntity;
 
+import javax.swing.*;
 import java.nio.file.Path;
 
 
 
-void main()
+void main() throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException
 {
+    try
+    {
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e)
+    {
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
     BaseEntity.setIdNumberWidth(4);
     Path linkerPath = Path.of("data", "Linker");
     Path entityPath = Path.of("data", "Entity");
     HospitalEntityAllocator hea = new HospitalEntityAllocator(linkerPath,entityPath);
     LoginForm loginForm = new LoginForm(hea);
     loginForm.setVisible(true);
+
 }
