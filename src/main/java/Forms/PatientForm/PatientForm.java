aec03e3b7204c45f8ed20414975ad8fef190ca17
JX <jingxian0426@gmail.com>
2026-09-26 21:49:18 +0800

    Delete log out button

--- src/main/java/Forms/PatientForm/PatientForm.java
+++ src/main/java/Forms/PatientForm/PatientForm.java
@@ -53,16 +53,6 @@ public class PatientForm extends javax.swing.JFrame {
      */
     public PatientForm() {
         initComponents();
-        logOutBtn.addActionListener(evt -> {
-            int result = javax.swing.JOptionPane.showConfirmDialog(this,
-                    "Are you sure you want to log out?", "Confirm Logout",
-                    javax.swing.JOptionPane.YES_NO_OPTION);
-            if (result != javax.swing.JOptionPane.YES_OPTION) {
-                return;
-            }
-            // TODO: Navigate to the login screen once it is merged.
-            dispose();
-        });
     }
 
     public PatientForm(HospitalEntityAllocator allocator, Patient patient) {
